7e9bb4441572ea45e32ab401a29aa9355956c6a2
Omar <93488662+Mangostar1@users.noreply.github.com>
2025-06-09 21:39:19 -0400

    Se mejora el código para que sea más legible y eficiente.

--- js/index.js
+++ js/index.js
@@ -1,16 +1,22 @@
 function handleSubmit(event) {
   event.preventDefault();
-  const email = document.getElementById('email').value;
+  const email = document.getElementById("email").value;
 
-  axios.post('https://back-web-page-financiera-production.up.railway.app/early-access/subscriptions', { email })
-    .then(response => {
-      console.log('Subscription successful:', response.data);
-      console.log('Subscripcion mensaje:', response.data.message);
-      console.log('Subscripcion email:', response.data.subscriber.email);
-      alert(`¡Gracias por registrarte, ${response.data.subscriber.email}! Te avisaremos pronto.`);
+  axios
+    .post(
+      "https://back-web-page-financiera-production.up.railway.app/early-access/subscriptions",
+      { email }
+    )
+    .then((response) => {
+      /* console.log("Subscripcion mensaje:", response.data.message); */
+      Swal.fire({
+        title: `¡Registro exitoso!`,
+        text: `Gracias por registrarte, ${email}. Te notificaremos pronto con más información.`,
+        icon: "success",
+      });
     })
-    .catch(error => {
-      console.error('Error during subscription:', error);
+    .catch((error) => {
+      console.error("Error during subscription:", error);
     });
-  document.getElementById('email').value = '';
+  document.getElementById("email").value = "";
 }
