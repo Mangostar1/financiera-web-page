866f66da54d28bf794bf2b4b6746426cc067da7c
Omar <93488662+Mangostar1@users.noreply.github.com>
2025-06-09 20:40:47 -0400

    Se hagrega axios.post para enviar los datos del formulario

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 function handleSubmit(event) {
   event.preventDefault();
   const email = document.getElementById('email').value;
-  alert(`¡Gracias por registrarte, ${email}! Te avisaremos pronto.`);
+
+  axios.post('https://back-web-page-financiera-production.up.railway.app/early-access/subscriptions', { email })
+    .then(response => {
+      console.log('Subscription successful:', response.data);
+      console.log('Subscripcion mensaje:', response.data.message);
+      console.log('Subscripcion email:', response.data.subscriber.email);
+      alert(`¡Gracias por registrarte, ${response.data.subscriber.email}! Te avisaremos pronto.`);
+    })
+    .catch(error => {
+      console.error('Error during subscription:', error);
+    });
   document.getElementById('email').value = '';
 }
